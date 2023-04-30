841e46cd674873a5aa5cad6ccbb9cb33393a4c7f
Vernon Miller <vernon.w.miller@gmail.com>
2023-04-29 21:49:05 -0600

    model updates

--- tui/inputui/model.go
+++ tui/inputui/model.go
@@ -1,8 +1,14 @@
 package inputui
 
 import (
+	"fmt"
+	"github.com/aldernero/timebox/tui/constants"
+	"github.com/aldernero/timebox/util"
 	"github.com/charmbracelet/bubbles/textinput"
 	tea "github.com/charmbracelet/bubbletea"
+	"os"
+	"strings"
+	"time"
 )
 
 type promptType int
@@ -12,17 +18,69 @@ const (
 	spanInput
 )
 
-type InputPrompt struct {
-	mode        promptType
-	inputs      []textinput.Model
-	inputStatus string
+type inputFields int
+
+const (
+	inputNameField inputFields = iota
+	inputMinField
+	inputMaxField
+	inputCancelButton
+	inputSubmitButton
+)
+
+type state int
+
+const (
+	normal state = iota
+	focused
+	disabled
+)
+
+type Model struct {
+	mode           promptType
+	state          state
+	focusedField   inputFields
+	editMode       bool
+	inputs         []textinput.Model
+	inputStatus    string
+	nameFieldTitle string
+	minFieldTitle  string
+	maxFieldTitle  string
+	isNameEditable bool
 }
 
-func (m InputPrompt) Init() tea.Cmd {
+func New() Model {
+	var m Model
+	m.inputs = make([]textinput.Model, 3)
+	var t textinput.Model
+	for i := range m.inputs {
+		t = textinput.New()
+		t.PromptStyle = constants.NoStyle
+		t.CursorStyle = constants.NoStyle
+		t.CharLimit = 30
+		switch i {
+		case 0:
+			t.Placeholder = "Box Name"
+			t.Focus()
+			t.PromptStyle = constants.FocusedStyle
+			t.TextStyle = constants.FocusedStyle
+		case 1:
+			t.Placeholder = "Weekly Min (e.g. 1h30m)"
+			t.CharLimit = 30
+		case 2:
+			t.Placeholder = "Weekly Max (e.g. 4h)"
+			t.CharLimit = 30
+		}
+		m.inputs[i] = t
+	}
+	return m
+}
+
+func (m Model) Init() tea.Cmd {
 	return nil
 }
 
-func (m InputPrompt) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	var cmds []tea.Cmd
 	var cmd tea.Cmd
 	switch msg := msg.(type) {
@@ -42,6 +100,85 @@ func (m InputPrompt) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	return m, tea.Batch(cmds...)
 }
 
-func (m InputPrompt) View() string {
+func (m Model) View() string {
 	return ""
+}
+
+func (m Model) inputView() string {
+	var b strings.Builder
+	b.WriteString(constants.InputTitleStyle.Render("New Box") + "\n")
+	for i := range m.inputs {
+		b.WriteString(m.inputs[i].View())
+		if i < len(m.inputs)-1 {
+			b.WriteRune('\n')
+		}
+	}
+
+	cancelButton := &constants.BlurredStyle
+	if int(m.focusedField) == len(m.inputs) {
+		cancelButton = &constants.FocusedStyle
+	}
+	submitButton := &constants.BlurredStyle
+	if int(m.focusedField) == len(m.inputs)+1 {
+		submitButton = &constants.FocusedStyle
+	}
+	_, err := fmt.Fprintf(
+		&b,
+		"\n\n%s  %s\n\n%s",
+		cancelButton.Render("[ Cancel ]"),
+		submitButton.Render("[ Submit ]"),
+		constants.ErrStyle(m.inputStatus),
+	)
+	if err != nil {
+		fmt.Printf("Error formatting input string: %v\n", err)
+		os.Exit(1)
+	}
+
+	return constants.PromptStyle.Render(constants.InputStyle(b.String()))
+}
+
+func (m Model) updateInputs() []tea.Cmd {
+	cmds := make([]tea.Cmd, len(m.inputs))
+	for i := 0; i <= len(m.inputs)-1; i++ {
+		if i == int(m.focusedField) {
+			// Set focused state
+			cmds[i] = m.inputs[i].Focus()
+			m.inputs[i].PromptStyle = constants.FocusedStyle
+			m.inputs[i].TextStyle = constants.FocusedStyle
+			continue
+		}
+		// Remove focused state
+		m.inputs[i].Blur()
+		m.inputs[i].PromptStyle = constants.NoStyle
+		m.inputs[i].TextStyle = constants.NoStyle
+	}
+	return cmds
+}
+
+func (m Model) resetInputs() {
+	m.inputs[inputNameField].Reset()
+	m.inputs[inputMinField].Reset()
+	m.inputs[inputMaxField].Reset()
+	m.focusedField = inputNameField
+	m.inputStatus = ""
+}
+
+func (m Model) validateInputs() (util.Box, error) {
+	var box util.Box
+	name := m.inputs[0].Value()
+	min := m.inputs[1].Value()
+	max := m.inputs[2].Value()
+	if name == "" || min == "" || max == "" {
+		return box, fmt.Errorf("empty fields")
+	}
+	minTime, err := time.ParseDuration(min)
+	if err != nil {
+		return box, fmt.Errorf("invalid duration: %v", err)
+	}
+	maxTime, err := time.ParseDuration(max)
+	if err != nil {
+		return box, fmt.Errorf("invalid duration: %v", err)
+	}
+	box = util.Box{Name: name, MinTime: minTime, MaxTime: maxTime}
+	return box, nil
 }
